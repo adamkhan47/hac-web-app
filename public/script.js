aaf8d2e40ee15037c0b305f4a1ca18fe845e4258
Adam Khan <adammkhan47@gmail.com>
2025-08-17 21:24:44 -0500

    set up login and stuff

--- public/script.js
+++ public/script.js
@@ -1,12 +1,21 @@
 let user = "";
 let password = "";
-let storage = localStorage.getItem(data);
 window.onload = function() {
-    if(storage === null) {
-        login();
+    try {
+        user = localStorage.getItem(login)[0];
+        password = localStorage.getItem(login)[1];
+    }
+    catch(error) {
+       login(); 
+       console.log(error);
     }
 };
 function login() {
     user = prompt("Enter your HAC Username");
     password = prompt("Enter your HAC Password");
+    if (user === null || password === null) {}
+    else {
+        let data = [user,password];
+        localStorage.setItem("login", data);
+    }
 }
