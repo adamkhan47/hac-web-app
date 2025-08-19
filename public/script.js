18e103e2b43fdc70c8befe1b19bfa0d8827568ce
Adam Khan <adammkhan47@gmail.com>
2025-08-18 19:27:24 -0500

    dont change variable early

--- public/script.js
+++ public/script.js
@@ -13,11 +13,12 @@ window.onload = function() {
     }
 };
 function login() {
-    user = prompt("Enter your HAC Username");
-    if (user === null) {return;}
-    password = prompt("Enter your HAC Password");
-    if (password === null) {return;}
-    let data = [user,password];
+    let userr = prompt("Enter your HAC Username");
+    if (userr === null) {return;}
+    let passwordd = prompt("Enter your HAC Password");
+    if (passwordd === null) {return;}
+    let data = [userr,passwordd];
+    user = userr; password = passwordd;
     localStorage.setItem("login", JSON.stringify(data));
     document.getElementById("user").innerHTML = user;
 }
