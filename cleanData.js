9a8a7df272839c8beb17e5c013bc8eac5620fcf2
Adam Khan <adammkhan47@gmail.com>
2025-08-19 20:55:28 -0500

    clean data WAY more up

--- cleanData.js
+++ cleanData.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const { convert } = require('html-to-text');
 // Whole point is to clean data from scraper.js into a readable format
 
 function cleanGrades(data) {
     let thing = data;
     thing = thing.substring(thing.indexOf('<div class="AssignmentClass">'), thing.length);
+    thing = convert(thing);
     fs.writeFileSync("gradeData.txt", thing);    
     return thing;
 }
